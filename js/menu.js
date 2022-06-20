47c6bf1347dea845738c5c54f96793cffce9be5b
Herman <smurovnemez@mail.ru>
2022-06-20 22:21:42 +0300

    Add-API-cards-partners

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,21 @@
 const menu = () => {
     const restourant = 'food-band.json'
+    const cardMenu = document.querySelector('.cards-menu')
 
     const renderItems = (data) => {
+        data.forEach(({ id, name, description, price, image }) => {
 
+            const cards = document.createElement('a')
+
+            console.log(cards)
+
+
+
+
+
+        });
+
+        // cardMenu.append(cards)
     }
 
     fetch(`./db/${restourant}`)
--- js/partners.js
+++ js/partners.js
@@ -1,24 +1,49 @@
     const partners = () => {
         const cardsRest = document.querySelector('.cards-restaurants')
-        const cardRest = document.querySelector('.card-restaurant')
+
 
         const renderItems = (data) => {
 
-            data.forEach(element => {
+            data.forEach(({ image, kitchen, name, price, products, stars, time_of_delivery }) => {
                 const a = document.createElement('a');
 
                 a.setAttribute('href', '/restaurant.html')
+                a.classList.add('card')
                 a.classList.add('card-restaurant')
+                a.dataset.products = products
 
-                console.log(a)
+                a.innerHTML = `
+                
+                <a href="restaurant.html" class="card card-restaurant">
+                        <img src="${image}" alt="" class="card-image" />
+                        <div class="card-text">
+                            <div class="card-heading">
+                                <h3 class="card-title">  ${name}</h3>
+                                <span class="card-tag tag">${time_of_delivery} мин</span>
+                            </div>
+                            <div class="card-info">
+                                <div class="rating">
+                                ${stars}
+                                </div>
+                                <div class="price">${price} ₽</div>
+                                <div class="category">${kitchen}</div>
+                            </div>
+                        </div>                   
+                    </a>
+                `
+
+                cardsRest.append(a)
+
 
             });
 
         }
 
-        fetch('./db/partners.json')
+        fetch('https://testdelivery-d0ff4-default-rtdb.firebaseio.com/db/partners.json')
             .then((response) => response.json())
-            .then((data) => { renderItems(data) })
+            .then((data) => {
+                renderItems(data)
+            })
             .catch((error) => {
                 console.log(error)
             })
